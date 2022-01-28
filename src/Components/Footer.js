f727735dd22efab81ae4142f038e4152ace86fa9
Daniel Mathiesen <danielverdugomathiesen@gmail.com>
2022-01-28 16:26:22 +0100

    Add first part of homepage

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+const footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li><a href="#">TemplateText</a></li>
+                <li><a href="#">TemplateText</a></li>
+                <li><a href="#">TemplateText</a></li>
+                <li><a href="#">TemplateText</a></li>
+            </ul>
+            <p>Template Text</p>
+        </footer>
+    );
+}
+ 
+export default footer
